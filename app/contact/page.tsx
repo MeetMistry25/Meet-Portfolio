7bb060cb57541703baaa621e30e8258e38132c74
Meetkumar Mistry <meetkumarmistry25@gmail.com>
2026-01-29 13:40:46 +0530

    Third Commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -48,8 +48,16 @@ export default function ContactPage() {
                             <Link href="https://www.linkedin.com/in/meetkumar-mistry-97501126b" className="p-3 bg-white/5 rounded-full text-zinc-400 hover:text-white hover:bg-white/10 transition-all">
                                 <Linkedin size={20} />
                             </Link>
-                            <Link href="https://twitter.com" className="p-3 bg-white/5 rounded-full text-zinc-400 hover:text-white hover:bg-white/10 transition-all">
-                                <Twitter size={20} />
+                            <Link href="https://x.com/MMistry_25" className="p-3 bg-white/5 rounded-full text-zinc-400 hover:text-white hover:bg-white/10 transition-all">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                                </svg>
                             </Link>
                         </div>
                     </div>
